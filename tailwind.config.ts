db1ff6bb47912b1c286c3db3265d65ddaba291f0
Almario Miano <almariomiano@gmail.com>
2025-04-21 12:24:10 +0800

    installed tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/quasar/src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
