2620907f89aad0cf15d8b8aa1b893d119f42b57b
Almario Miano <almariomiano@gmail.com>
2025-05-09 16:16:17 +0800

    Added website link and version

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -5,9 +5,21 @@
     </q-page-container>
 
     <q-footer class="bg-transparent text-grey-7 flex justify-center">
-      <div>&copy; 2025</div>
+      <div>
+        <span class="cursor-pointer" @click="openLink">{{ productName }}</span>
+        v{{ version }} &copy; 2025
+      </div>
     </q-footer>
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { version, productName } from '../../package.json';
+
+const host = computed(() => import.meta.env.VITE_API);
+
+const openLink = () => {
+  window.open(host.value, '_blank');
+};
+</script>
