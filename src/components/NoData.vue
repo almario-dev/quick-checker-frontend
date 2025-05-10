cb25f36bac5829201164802a946b822820fd1718
Almario Miano <almariomiano@gmail.com>
2025-05-10 09:57:10 +0800

    Import NoData png

--- src/components/NoData.vue
+++ src/components/NoData.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="column justify-center items-center py-6 gap-4">
     <q-avatar square size="5rem">
-      <q-img src="src/assets/no-data.png" height="5rem" />
+      <q-img :src="NoDataPng" height="5rem" />
     </q-avatar>
 
     <span class="text-[1rem] text-grey-8">No Data Available.</span>
@@ -10,4 +10,6 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NoDataPng } from './images';
+</script>
--- src/components/images.ts
+++ src/components/images.ts
@@ -5,3 +5,4 @@ export { default as TestPng } from 'src/assets/test.png';
 export { default as UI1 } from 'src/assets/ui1.png';
 export { default as EmptySvg } from 'src/assets/svg/empty.svg';
 export { default as ErrorImgSvg } from 'src/assets/error-img.jpg';
+export { default as NoDataPng } from 'src/assets/no-data.png';
