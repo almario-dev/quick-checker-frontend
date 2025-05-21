b71468f376c92667542526fc1f52cd67a275b4b3
Almario Miano <almariomiano@gmail.com>
2025-05-21 20:25:00 +0800

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  important: true,
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/quasar/src/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
